5cf6fdd406a0f18ecf0078e5fbb42c77b332ef78
XxGonanxX <patomorelia@hotmail.com>
2023-09-01 17:01:36 -0600

    Se crea el .py para correciones de algoritmo

--- TEST_ALAN.py
+++ TEST_ALAN.py
@@ -1,216 +1,201 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
-import heapq
-import random
+from mesa.space import SingleGrid
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import heapq
+import random
+import os
+
+# Obtener la ubicación del directorio actual del script
+script_dir = os.path.dirname(__file__)
+
+# Construir la ruta completa al archivo 'mapa.txt'
+mapa_file = os.path.join(script_dir, 'mapa.txt')
 plt.rcParams["animation.html"] = "jshtml"
 matplotlib.rcParams['animation.embed_limit'] = 2**128
+import pandas as pd
+from mesa.datacollection import DataCollector
+from IPython.display import HTML
+import heapq
+import numpy as np
 import networkx as nx
 
-import numpy as np
-import pandas as pd
+# ESTE COMANDO ES PARA LOS PRINTS, PARA VER QUE SALGAN CORRECTO Y ASÍ, ELIMINAR AL FINALIZAR
+from IPython.display import display
 
-# Función para leer el mapa desde un archivo de texto.
 def leer_mapa(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
-        mapa = [list(line.replace(" ", "").strip()) for line in lines[1:]]
-    
-    return mapa
+        mapaREAL = [list(line.replace(" ", "").strip()) for line in lines[1:]]
 
+    filas = len(mapaREAL)
+    columnas = len(mapaREAL[0])
 
-
-def construir_grafo(mapa):
-    grafo = {}
-    filas = len(mapa)
-    columnas = len(mapa[0])
+    mapaUNKNOWN = [fila[:] for fila in mapaREAL]  # Crear una copia de mapaREAL
 
     for i in range(filas):
         for j in range(columnas):
-            if mapa[i][j] != 'X':  # Si la celda no es un obstáculo.
-                nodo = (i, j)
-                vecinos = []  # Lista para almacenar vecinos accesibles.
-
-                # Verifica y agrega vecinos accesibles.
-                if i - 1 >= 0 and mapa[i-1][j] != 'X': vecinos.append((i-1, j))
-                if i + 1 < filas and mapa[i+1][j] != 'X': vecinos.append((i+1, j))
-                if j - 1 >= 0 and mapa[i][j-1] != 'X': vecinos.append((i, j-1))
-                if j + 1 < columnas and mapa[i][j+1] != 'X': vecinos.append((i, j+1))
-
-                grafo[nodo] = vecinos  # Asigna los vecinos al nodo en el grafo.
+            if mapaREAL[i][j] not in ['S', 'P']:
+                mapaUNKNOWN[i][j] = '?'
     
     
-    # Se dibuja el grafo
+#    SE IMPRIMEN LOS MAPAS EN TERMINAL
+#
+#    print("MapaREAL:")
+#    for fila in mapaREAL:
+#        print(' '.join(fila))
+#    
+#    print("MapaUNKNOWN:")
+#    for fila in mapaUNKNOWN:
+#        print(' '.join(fila))
     
-    G = nx.DiGraph()
+    return mapaREAL, mapaUNKNOWN
 
-    # Agrega los nodos al grafo
-    for nodo, vecinos in grafo.items():
-        G.add_node(nodo)
-        for vecino in vecinos:
-            G.add_edge(nodo, vecino)
-
-    # Dibuja el grafo utilizando matplotlib
-    pos = {nodo: nodo for nodo in G.nodes()}  # Definir la posición de los nodos para que se dibujen en sus coordenadas
-    nx.draw(G, pos, with_labels=True, node_size=1000, font_size=10, node_color='lightblue', font_color='black')
-
-    # Muestra el dibujo del grafo
-    plt.show()
-    
-    # Recuerda que el código no seguirá ejecutándose hasta que cierres la ventana del grafo.
-
-    return grafo
-
-
-
-# Función heurística: distancia de Manhattan
-def heuristic(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-# Implementación del algoritmo A*
-def a_star_search(graph, start, goal):
-    frontier = []
-    heapq.heappush(frontier, (0, start))
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-
-    while frontier:
-        _, current = heapq.heappop(frontier)
-
-        if current == goal:
-            break
-
-        for next in graph[current]:
-            new_cost = cost_so_far[current] + 1
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                heapq.heappush(frontier, (priority, next))
-                came_from[next] = current
-
-    # Reconstruct path
-    current = goal
-    path = []
-    while current != start:
-        path.append(current)
-        current = came_from[current]
-    path.append(start)
-    path.reverse()
-    
-    return path
 
 class Robot(Agent):
-    def __init__(self, unique_id, model, inicio):
+    def __init__(self, unique_id, model, inicio, mapaREAL, mapaUNKNOWN):
         super().__init__(unique_id, model)
         self.posicion = inicio
         self.capacidad = 5
         self.memoria = {}
+        self.memoria[inicio] = 'S'
         self.basura_no_recogida = None
+        self.destinos_pendientes = [0]
+        self.mapaREAL = mapaREAL
+        self.mapaUNKNOWN = mapaUNKNOWN
 
     def step(self):
-        grafo = self.model.grafo
-        mapa = self.model.mapa
+        self.mapeo()
+        self.moverse()
+        print("Ahora estoy en la posición:")
+        print(self.posicion)
+        print("Mi memoria es:")
+        print(self.memoria)
 
-        # Encuentra la basura más cercana usando A*
-        basura_cercana = None
-        camino_corto = None
+    def mapeo(self):
+        print("Estoy mapeando chucuchú")
+        if self.memoria[self.posicion] == 'B':
+            print("Exploro detalles VAMOOOS")
+            self.explorar_detalle()
+
+        self.memoria[self.posicion] = self.mapaREAL[self.posicion[0]][self.posicion[1]] 
+
+        # Actualizar mapaUNKNOWN con información del mapaREAL
+        self.mapaUNKNOWN[self.posicion[0]][self.posicion[1]] = self.mapaREAL[self.posicion[0]][self.posicion[1]]
+
+        # Actualizar memoria con información del entorno
+        vecinos = self.model.grid.get_neighborhood(self.posicion, moore=True, include_center=True)
+        for vecino in vecinos:
+        #    print(f"Analizando vecino: {vecino}")
+            contenido = self.model.mapa[vecino[1]][vecino[0]]  # Corrección aquí
+        #    print(f"Contenido: {contenido}")
+            if contenido == 'X':
+                self.memoria[vecino] = 'X'
+            if contenido == '0':
+                self.memoria[vecino] = '0'
+            if contenido.isdigit():
+                self.memoria[vecino] = 'B'
+            else:
+                self.memoria[vecino] = contenido
+
+        # Actualizar celda actual en la grid
+        self.model.grid[self.posicion[0]][self.posicion[1]] = self.memoria[self.posicion]
+
+
+    def explorar_detalle(self):
+        contenido = self.model.mapa[self.posicion[0]][self.posicion[1]]
+    
+        if contenido == 'X':
+            print("Se reveló que hay un obstáculo en esta celda.")
+            self.memoria[self.posicion] = 'X'
+        elif contenido.isdigit():
+            print(f"Se reveló que hay {contenido} de basura en esta celda.")
+            valor_basura = int(contenido)
+            self.memoria[self.posicion] = valor_basura
+            self.model.mapa[self.posicion[0]][self.posicion[1]] = str(valor_basura)  # Actualiza también en el mapa
+            self.model.grid[self.posicion[0]][self.posicion[1]] = str(valor_basura)  # Actualiza también en la grid
+            print(self.memoria)
+        else:
+            print("No se reveló nada nuevo en esta celda.")
+                
+    
+    def obtener_vecinos_conocidos(self):
+        vecinos = self.model.grid.get_neighborhood(self.posicion, moore=True, include_center=False)
+        vecinos_conocidos = [vecino for vecino in vecinos if vecino in self.memoria and self.memoria[vecino] != '?']
+        return vecinos_conocidos
         
-        # Va a tardar mil años, hay que tratar de optimizarlo
+    def moverse(self):
+        print("Si me muevo")
+        destino = self.destinos_pendientes[0]
+        if self.posicion == destino:
+            self.destinos_pendientes.pop(0)
+            return
+
+        ruta = self.planificar_ruta(destino)
+        siguiente_celda = ruta[0]
+        self.posicion = siguiente_celda
+        self.model.grid.move_agent(self, siguiente_celda)
         
-        for i in range(len(mapa)):
-            for j in range(len(mapa[0])):
-                if mapa[i][j].isdigit() and int(mapa[i][j]) > 0:
-                    camino = a_star_search(grafo, self.posicion, (i, j))
-                    if camino_corto is None or len(camino) < len(camino_corto):
-                        camino_corto = camino
-                        basura_cercana = (i, j)
+    def planificar_ruta(self, destino):
+        cola = [(self.posicion, [])]  # Cola para el BFS
+        print("Imprimo la cola")
+        print(cola)
+        visitados = set()
 
-        # Moverse hacia la basura más cercana
-        if camino_corto and len(camino_corto) > 1:
-            nueva_posicion = camino_corto[1]
-            self.posicion = nueva_posicion
+        while cola != []:
+            celda_actual, ruta_actual = cola.pop(0)
+            if celda_actual == destino:
+                print("Imprimo la ruta_actual")
+                print(ruta_actual)
+                return ruta_actual
 
-            # Recoger basura
-            if mapa[nueva_posicion[0]][nueva_posicion[1]].isdigit() and int(mapa[nueva_posicion[0]][nueva_posicion[1]]) > 0:
-                basura = min(int(mapa[nueva_posicion[0]][nueva_posicion[1]]), self.capacidad)
-                self.capacidad -= basura
-                mapa[nueva_posicion[0]][nueva_posicion[1]] = str(int(mapa[nueva_posicion[0]][nueva_posicion[1]]) - basura)
+            if celda_actual in visitados:
+                continue
 
-                # Si no pudo recoger toda la basura, almacena la posición
-                if int(mapa[nueva_posicion[0]][nueva_posicion[1]]) > 0:
-                    self.basura_no_recogida = nueva_posicion
+            visitados.add(celda_actual)
+            vecinos_conocidos = self.obtener_vecinos_conocidos()
+            vecinos_libres = [vecino for vecino in vecinos_conocidos if self.memoria[vecino] != 'X']
 
-        # Comunicación entre robots
-        for agent in self.model.schedule.agents:
-            if isinstance(agent, Robot) and agent != self:
-                self.memoria.update(agent.memoria)
-                agent.memoria.update(self.memoria)
-
-                # Comparte la posición de basura no recogida
-                if self.basura_no_recogida:
-                    agent.memoria[self.basura_no_recogida] = 'basura_no_recogida'
+            for vecino in vecinos_libres:
+                cola.append((vecino, ruta_actual + [vecino]))
+                
+        return ruta_actual
 
 class LimpiezaModel(Model):
     def __init__(self, mapa_txt):
-        self.num_robots = 5
+        super().__init__(self)
+        self.datacollector = DataCollector(
+            model_reporters={"Mapa": "mapa"},
+            agent_reporters={"Posicion": "posicion"})
+        self.num_robots = 1
         self.schedule = RandomActivation(self)
-        self.mapa = leer_mapa(mapa_txt)
-        self.grafo = construir_grafo(self.mapa)
+        self.mapa, self.mapaREAL = leer_mapa(mapa_txt)
+        self.mapaUNKNOWN = [fila[:] for fila in self.mapa]  # Copia del mapa desconocido
+        self.grid = SingleGrid(len(self.mapa), len(self.mapa[0]), torus=False)
+        self.bote_basura = ()
+        self.basura = ()
+        self.obstaculo = ()
+        self.mapa, self.mapaREAL = leer_mapa(mapa_txt)
+        self.memoria_mapa = {}
+        self.todas_celdas_mapeadas = 1
+
+        # self.fig, self.ax = plt.subplots()
 
         # Creación de robots
         for i in range(self.num_robots):
-            robot = Robot(i, self, (0, 0))
+            robot = Robot(i, self, (0, 0), self.mapaREAL, self.mapaUNKNOWN)
             self.schedule.add(robot)
+    
+            self.grid.place_agent(robot, (0, 0))
 
     def step(self):
+        print("Paso")   
         self.schedule.step()
-
-def main():
-    #Cambiar los steps a un while 'Hay_Basura' para que no se deje de buscar
-    #que se haya recogido toda la basura.
-
-    model = LimpiezaModel('mapa.txt')
-    for _ in range(10000):
-        model.step()
-
-    # Imprime el mapa después de la simulación
-    print("Mapa después de la simulación:")
-    for fila in model.mapa:
-        print("".join(fila))
-    #representar graficamente con matplotlib, o alguna otra herramienta.
-
-if __name__ == "__main__":
-    main()
-    
-    
-    
-# Grid para la visualización
-
-def get_grid(model):
-    grid = np.zeros((model.grid.width, model.grid.height, 3))  # Usamos una matriz de 3 dimensiones para almacenar colores
         
-    # Recorremos la grilla
-    for cell in model.grid.coord_iter():
-        cell_content, (x, y) = cell
-        if cell_content is not None:
-            cell_agent = cell_content
-        elif isinstance(cell_agent, Robot):
-            grid[x][y] = [0, 0, 1]  # Azul para los tiburones
-        else:
-            grid[x][y] = [1, 1, 1]  # Blanco para los espacios vacíos
-                    
-    return grid
+model = LimpiezaModel(mapa_file)
 
-all_grid = model.datacollector.get_model_vars_dataframe()
-
-colored_grid = get_grid(model)
-
-# Mostrar la grilla usando matplotlib
-plt.imshow(colored_grid)
-
-anim = animation.FuncAnimation(plt.gcf(), lambda i: plt.imshow(all_grid.iloc[i][0]), frames=len(all_grid))
-anim.save('ROOMBA.gif', writer='ffmpeg', fps=10)
+steps = 4
+for i in range(steps): 
+    model.step()
