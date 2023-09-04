e6c9cb44426036b6746161378e37c8e71c9954b5
XxGonanxX <patomorelia@hotmail.com>
2023-09-03 22:50:12 -0600

    Se comenta el código de test para mejor entendimiento

--- test.py
+++ test.py
@@ -32,6 +32,7 @@ def leer_mapa(filename):
     filas = len(mapaREAL)
     columnas = len(mapaREAL[0])
 
+    # Este segundo mapa, es el que tendrá el símbolo ?, es el que usará el robot para el mapeo
     mapa = [fila[:] for fila in mapaREAL]  # Crear una copia de mapaREAL
 
     for i in range(filas):
@@ -41,6 +42,7 @@ def leer_mapa(filename):
 
     
 #    SE IMPRIMEN LOS MAPAS EN TERMINAL
+
     print("MapaREAL:")
     for fila in mapaREAL:
         print(' '.join(fila))
@@ -70,10 +72,13 @@ class Robot(Agent):
 
     def mapeo(self):
         print("Estoy mapeando chucuchú")
+        
+        # Si la celda actual es una basura 'B', se actualiza la memoria con su valor real
         if self.model.memoria[self.posicion] == 'B':
             print("Exploro detalles VAMOOOS")
             self.explorar_detalle()
 
+        # La memoria se actualiza con la información del mapaREAL
         self.model.memoria[self.posicion] = self.model.mapaREAL[self.posicion[0]][self.posicion[1]] 
 
         # Actualizar mapa con información del mapaREAL
@@ -81,9 +86,11 @@ class Robot(Agent):
 
         # Actualizar memoria con información del entorno
         vecinos = self.model.grid.get_neighborhood(self.posicion, moore=True, include_center=True)
+        
+        # Si hay X se queda como X, si es un valor diferente a 0 se convierte en B y si es 0 se convierte en 0
         for vecino in vecinos:
             print(f"Analizando vecino: {vecino}")
-            contenido = self.model.mapa[vecino[1]][vecino[0]]  # Corrección aquí
+            contenido = self.model.mapa[vecino[1]][vecino[0]] 
             print(f"Contenido: {contenido}")
             if contenido == 'X':
                 self.model.memoria[vecino] = 'X'
@@ -102,6 +109,12 @@ class Robot(Agent):
         if contenido.isdigit():
             print(f"Se reveló que hay {contenido} de basura en esta celda.")
             valor_basura = int(contenido)
+            
+            ###############
+            # FALLA AQUÍ  #
+            ###############
+            
+            # Se actualiza la memoria con el valor real de la basura
             self.model.memoria[self.posicion] = valor_basura
             self.model.mapa[self.posicion[0]][self.posicion[1]] = str(valor_basura)  # Actualiza también en el mapa
             self.model.grid[self.posicion[0]][self.posicion[1]] = str(valor_basura)  # Actualiza también en la grid
@@ -109,27 +122,35 @@ class Robot(Agent):
         else:
             print("No se reveló nada nuevo en esta celda.")
         
-        
-                
+            ###############
+            # FIN FALLA   #
+            ###############
     
     def obtener_vecinos_conocidos(self):
+        # Obtiene los vecinos conocidos de la celda actual
         vecinos = self.model.grid.get_neighborhood(self.posicion, moore=True, include_center=False)
         vecinos_conocidos = [vecino for vecino in vecinos if vecino in self.model.memoria and self.model.memoria[vecino] != '?']
         return vecinos_conocidos
         
     def moverse(self):
         print("Si me muevo")
+        # Si no hay destinos pendientes, se elige uno al azar
         destino = self.destinos_pendientes[0]
         if self.posicion == destino:
+            # Si ya llegó al destino, se elimina de la lista de destinos pendientes
             self.destinos_pendientes.pop(0)
             return
-
+        
+        # Si no, se planifica la ruta al destino
         ruta = self.planificar_ruta(destino)
         print("Imprimo la ruta")
         print(ruta)
         siguiente_celda = ruta[0]
         self.posicion = siguiente_celda
         
+    ##########################
+    # SE SOSPECHA FALLA AQUÍ #
+    ##########################
         
     def planificar_ruta(self, destino):
         cola = [(self.posicion, [])]  # Cola para el BFS
@@ -137,30 +158,41 @@ class Robot(Agent):
         print(cola)
         visitados = set()
 
+        # BFS
         while cola != []:
+            # Se obtiene la celda actual y la ruta actual
             celda_actual, ruta_actual = cola.pop(0)
+            # Se agrega la celda actual a la ruta actual
             if celda_actual == destino:
                 print("Imprimo la ruta_actual")
                 print(ruta_actual)
                 return ruta_actual
-
+            
+            # Si la celda actual ya fue visitada, se ignora
             if celda_actual in visitados:
                 continue
 
+            # Si no, se agrega a la lista de visitados
             visitados.add(celda_actual)
             vecinos_conocidos = self.obtener_vecinos_conocidos()
             vecinos_libres = [vecino for vecino in vecinos_conocidos if self.model.memoria[vecino] != 'X']
 
+            # Se agregan los vecinos libres a la cola
             for vecino in vecinos_libres:
                 cola.append((vecino, ruta_actual + [vecino]))
                 
+        # Si no se encontró ruta, se devuelve la ruta actual
         return ruta_actual
     
+    ########################
+    # FIN FALLA SOSPECHADA #
+    ########################
 # Se importa el modelo
 
 class LimpiezaModel(Model):
     def __init__(self, mapa_txt):
         super().__init__(self)
+        # Se crea el mapa
         self.datacollector = DataCollector(
             model_reporters={"Mapa": "mapa"},
             agent_reporters={"Posicion": "posicion"})
@@ -193,8 +225,3 @@ model = LimpiezaModel('mapa.txt')
 steps = 2
 for i in range(steps): 
     model.step()
-
-# Se imprime el mapa final
-print("Mapa final:")
-for fila in model.mapa:
-    print(' '.join(fila))
