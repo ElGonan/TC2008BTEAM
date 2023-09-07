fd274deb580cdf21ae23529d6aa755b0819232fe
XxGonanxX <patomorelia@hotmail.com>
2023-09-06 22:08:07 -0600

    Se añade presentacion, se corrije docu, y se renombran de forma correcta ciertos archivos

--- Reto3D/Assets/Scripts/CameraController.cs
+++ Reto3D/Assets/Scripts/CameraController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float velocidadMovimiento = 5.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Obtén la entrada del teclado para el movimiento en los ejes X y Y
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        // Calcula el desplazamiento en los ejes X y Y
+        Vector3 desplazamiento = new Vector3(movimientoHorizontal, movimientoVertical, 0) * velocidadMovimiento * Time.deltaTime;
+
+        // Aplica el desplazamiento a la posición de la cámara
+        transform.Translate(desplazamiento);
     }
 }
